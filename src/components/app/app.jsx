76de12c56a42b8ba35e85b8848737ba9f2598fd8
damir <damir@gismeteo.ru>
2020-10-02 16:14:13 +0300

    add routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
+import AuthPage from '../auth-page/auth-page';
+import FavoritesPage from '../favorites-page/favorites-page';
 import MainPage from '../main-page/main-page';
+import OfferPage from '../offer-page/offer-page';
 
 const App = ({offersCount}) => {
   return (
-    <MainPage
-      offersCount={offersCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage
+            offersCount={offersCount}
+          />
+        </Route>
+        <Route exact path="/login">
+          <AuthPage />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
